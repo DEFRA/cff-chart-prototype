91bc75b93aa1c61912ece8eaaa81550cc9c42299
Nick Blows <nickblows@gmail.com>
2026-01-22 10:56:08 +0000

    removed redundant file

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -1,14 +1,13 @@
 import { initAll } from 'govuk-frontend'
 import './utils.js'
 import './toggletip.js'
-import { LineChart } from './line-chart.js'
+import { lineChart } from './line-chart.js'
 
 initAll()
 
 // Initialize chart if telemetry data is available
 if (typeof document !== 'undefined' && typeof window !== 'undefined') {
   if (document.getElementById('line-chart') && window.flood && window.flood.model) {
-    // eslint-disable-next-line no-new
-    new LineChart('line-chart', window.flood.model.id, window.flood.model.telemetry)
+    lineChart('line-chart', window.flood.model.id, window.flood.model.telemetry)
   }
 }
